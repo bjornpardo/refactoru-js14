59c2090eecdb3f20eff49486777a0525e9ba9028
Bjorn Pardo <bjorn@xend.com.ph>
2014-04-17 12:32:41 -0600

    Partial Bonus 1

--- main.js
+++ main.js
@@ -1,32 +1,53 @@
 $(document).on('ready', function() {
 
-	// $('div').on('click', function() {
- //  		alert('right here!');
- //  	});
-
-	$('.container').on('click', function(e) {
+	$('.map').on('click', function(e) {
 		
 		var marker = $('<div class="marker"></div>');
-		$(this).append(marker);
-		
+		$('.container').append(marker);
 
+		var textArea = $('<textarea></textarea>');
+		$(marker).after(textArea);
+
+		var okbtn = $('<button type='submit'>Submit</button>');
+		$(marker).after(textArea);
+			
 		var x = e.pageX;
 		var y = e.pageY;
 
-		// $( '.marker' ).offset({ top: x, left: y });
-
 		console.log(x, y);
+
+		$(marker).on('click', function() {
+		
+		$(this).remove();
+
+		});	
+
+		$(textArea).on('click', function() {
+		
+		$(this).remove();
+
+		});	
 
 
 		marker.css({
 			'left': x + 'px',
 			'top': y + 'px',
 			'background-color': 'red'
-			
-		});
-	});
-  		
+		});	
 
+		textArea.css({
+			'left': (x+5) + 'px',
+			'top': (y+5) + 'px',
+
+		});	
+
+	});
+	console.log(12)
+	// $(marker).on('click', function(e) {
+		
+	// $(this).remove();
+
+	// });	
   
 });
 
